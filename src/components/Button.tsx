af8970a1115e01baf0d788a26a12dd6e53784605
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-01-26 17:37:44 -0500

    add color in component Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 interface Props {
 	variant?: 'primary' | 'secondary' | 'text';
+	color?: 'error' | 'neutro' | 'primary';
 	icon?: React.ReactNode;
 	size?: 'small' | 'medium' | 'large';
 	children?: React.ReactNode;
@@ -9,12 +10,26 @@ interface Props {
 	disabled?: boolean;
 }
 
-const Button: React.FC<Props> = ({ children, variant = 'primary', ...rest }) => {
+const getVariableCss = (color: string): string => {
+	const variables: Record<string, string> = {
+		primary: '--primary-color',
+		secondary: '--color-secondary',
+		text: '--color-text',
+		error: '--soft-red',
+		neutro: '--grayish-blue',
+	};
+
+	return getComputedStyle(document.documentElement).getPropertyValue(variables[color]);
+};
+
+const Button: React.FC<Props> = ({ children, variant = 'primary', color = 'primary', ...rest }) => {
 	const isDisabled = Boolean(rest.disabled);
 
 	const className = `btn btn__${variant}`;
+	const btnColor = useMemo(() => getVariableCss(color), [color]);
+	
 	return (
-		<button className={className} disabled={isDisabled} {...rest}>
+		<button style={{ '--color': btnColor }} className={className} disabled={isDisabled} {...rest}>
 			{children}
 		</button>
 	);
