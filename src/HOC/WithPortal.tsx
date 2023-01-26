c75767af6bfa7db931b970b7391d7cf133819a6a
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-01-26 17:38:17 -0500

    create HOC portal

---
+++ src/HOC/WithPortal.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createPortal } from 'react-dom';
+
+
+const WithPortal = (WrappedComponent: React.FC, idElement:String) => {
+    return (props: any) => {
+      console.log(WrappedComponent, idElement, props)
+        const el = document.getElementById(idElement as string) as HTMLElement;
+        
+        return createPortal(<WrappedComponent {...props} />, el);
+    }
+}
+
+
+export default WithPortal
