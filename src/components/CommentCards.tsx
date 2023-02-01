1317aa6bd914bb6ddeb8ce85f9fda60ef71e69f1
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-02-01 12:35:14 -0500

    add tree comment card

--- src/components/CommentCards.tsx
+++ src/components/CommentCards.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Comment } from '../types/comments';
+import { CommentsWithChildren as Comment } from '../types/comments';
 import Avatar from './Avatar';
 import Button from './Button';
 import ReplyEditor from './Editor/ReplyEditor';
@@ -13,29 +13,35 @@ interface Props {
 const CommentCards: React.FC<Props> = ({ comment }) => {
 	return (
 		<>
-		<article className='comments-card'>
-			<div className='comments-card__aside'>
-				<Score initialScore={comment.score}></Score>
-			</div>
-			<div>
-				<header className='comments-card__header'>
-					<div className='coments-card__headerDetails'>
-						<Avatar src={comment.user.avatar} name={comment.user.userName} size='small'></Avatar>
-						<h3>{comment.user.userName}</h3>
-						<TimeStamp timestamp={comment.timestamp}></TimeStamp>
-					</div>
-					<div className='coments-card__actions'>
-						<Button variant='text'>
-							<img src='./icon-reply.svg' alt='icon reply' /> Reply
-						</Button>
-					</div>
-				</header>
-				<div className='comments-card__body'>
-					<p>{comment.comments}</p>
+			<article className='comments-card'>
+				<div className='comments-card__aside'>
+					<Score initialScore={comment.score}></Score>
 				</div>
+				<div>
+					<header className='comments-card__header'>
+						<div className='coments-card__headerDetails'>
+							<Avatar src={comment.user.avatar} name={comment.user.userName} size='small'></Avatar>
+							<h3>{comment.user.userName}</h3>
+							<TimeStamp timestamp={comment.timestamp}></TimeStamp>
+						</div>
+						<div className='coments-card__actions'>
+							<Button variant='text'>
+								<img src='./icon-reply.svg' alt='icon reply' /> Reply
+							</Button>
+						</div>
+					</header>
+					<div className='comments-card__body'>
+						<p>{comment.comments}</p>
+					</div>
+				</div>
+			</article>
+			<div className='commentLine'>
+				{comment?.children != null &&
+					comment.children.map(child => {
+						return <CommentCards key={child.id} comment={child} />;
+					})}
+				{/* <ReplyEditor/> */}
 			</div>
-		</article>
-		{/* <ReplyEditor/> */}
 		</>
 	);
 };
--- src/json/commentsdb.ts
+++ src/json/commentsdb.ts
@@ -64,6 +64,6 @@ export const commentsApp:Comment[] = [
     timestamp: '2023-01-01T12:00:00.000Z',
     comments:"@ramailesa3 I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newwest library/ framework. but the fundementals are what stay constant. ",
     score: 12,
-    parentId: 2
+    parentId: 4
   },
 ]
